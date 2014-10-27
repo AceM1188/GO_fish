100e9c19d77b82279a9429ffb365b12a039f2092
kevingmonahan <kgm2108@columbia.edu>
2014-10-27 02:27:06 +0000

    added formatting and p-value filter for cuffdiff

--- cgi_gofish.pl
+++ cgi_gofish.pl
@@ -1,25 +1,29 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use CGI ':standard';
+use CGI qw(:standard :html3);
+use CGI::Carp qw(fatalsToBrowser);
+
 my @data_types = qw (cuffdiff cufflinks CGH_array);
 my @search_modes = qw (AND OR);
 my @results;
+
 print header;
-print start_html('GO fishing with GO terms'),
-    h1('Search gene data with GO terms'),
+print start_html('GO fishing with GO terms'),   
+    '<div class="container-fluid">',h1('Search gene data with GO terms'),
     start_multipart_form,
     "Source organism genus (e.g. Homo, Mus, Drosophila):",br
     textarea(-name=>'Genus',-rows=>1,-cols=>40),br,br,
     h3("Input data"),
     "Data Type:  ", radio_group(-name=>'data_type',-value=>\@data_types,-default=>$data_types[0]),br,
+    "For cuffdiff: use a p-value cut-off instead of the cuffdiff signicance call", textfield(-name=>'p_value'),br,
     "Upload file here: ",filefield(-name=>'upload_data'),br,br, 
     h3("GO search"),
     "Search parameters:",br
-    "One per line, can use GO terms (GO:00000000) or key words (use single quotes to group key words)",br,
+    "One per line, can use GO terms (GO:0044822) or key words (use single quotes to group key words)",br,
     textarea(-name=>'GO_terms and search terms', -rows=>5, -cols=>40),br,br,
     "Search type:", radio_group(-name=>'Search_Mode', -value=>\@search_modes, -default=>$search_modes[0]),br,
-submit('Search'),
+submit('Search'),'</div>',
     end_form,
     hr;
 my $organism = param ('Genus');
@@ -29,6 +33,7 @@ my $search_mode = param ('Search_Mode');
 my $term = param ('GO_terms and search terms');
 my @terms = split (/\n/, $term);
 my $string = join(" ",  @terms);
+my $p_val = param ('p_value');
 
 #print h3("search terms:","@terms"), br
 #    "search string:",$string, br;
@@ -43,7 +48,7 @@ my $string = join(" ",  @terms);
 my $tmp = "/tmp/gofish.$$";
 open (OUT, '>', $tmp) or die "can't write";
 unless ($file_upload){
-    print h2("Please provide a data file."), br;
+    print '<div class="container-fluid">',h2("Please provide a data file."), br, '</div>';
     die;
 }
 
@@ -52,10 +57,54 @@ while (my $line = <$file_upload>){
 }
 #my $cmd = "GO_fish.pl --run_mode CGI --organism $organism --data $tmp --data_type $data_type --search_mode $search_mode --terms $string\n";
 #print h3($cmd), br;
-@results = `/home/gofish/GO_fish.pl --run_mode CGI --organism $organism --data $tmp --data_type $data_type --search_mode $search_mode --terms $string`;
-my $count = scalar @results;
-for (my $t=0; $t<$count; $t++){
-    print "$results[$t]",br;
+
+
+#@results = `/home/gofish/GO_fish.pl --run_mode CGI --organism $organism --data $tmp --data_type $data_type --search_mode $search_mode --terms $string`;
+#print h3($p_val),br;
+if ($p_val){
+    @results = `/home/gofish/GO_fish.pl --run_mode CGI --organism $organism --data $tmp --data_type $data_type --search_mode $search_mode --p_value $p_val --terms $string`;
 }
+else {
+    @results = `/home/gofish/GO_fish.pl --run_mode CGI --organism $organism --data $tmp --data_type $data_type --search_mode $search_mode --terms $string`;
+}
+
+
+
+my $count = scalar @results;
+
+#for (my $t=0; $t<$count; $t++){
+#    print "$results[$t]",br;
+#}
+
+for (my $t=0; $t<$count; $t++){
+    chomp $results[$t];
+    if ( $t==0 ){
+	my @lineterms = split("\t", $results[$t]);
+
+	print '<div class="container-fluid"><table border="3"><tr>',
+	'<th>',$lineterms[0],'</th>',
+	'<th>',$lineterms[1],'</th>',
+	'<th>',$lineterms[2],'</th>',
+	'<th>',$lineterms[3],'</th>',
+	'<th>',$lineterms[4],'</th>',
+	'</tr>';next;
+    }
+    my @lineterms = split("\t", $results[$t]);
+
+        print '<tr>',
+        '<td>',$lineterms[0],'</td>',
+        '<td>',$lineterms[1],'</td>',
+        '<td>',$lineterms[2],'</td>',
+        '<td>',$lineterms[3],'</td>',
+        '<td>',$lineterms[4],'</td>',
+    '</tr>';
+}
+print '</div>';
+    
+#    }}
+#}
+#    my @lineterms = split("\t", $results[$t]);
+#    foreach my $stuff (@lineterms
+
 #print "@results", br;
 print end_html;
